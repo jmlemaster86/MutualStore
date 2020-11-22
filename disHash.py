7460907cebcaf86d348701370a7a773ada99e633
jmlemaster86 <jmlemaster86@gmail.com>
2020-11-22 19:56:45 +0000

    .

--- disHash.py
+++ disHash.py
@@ -2,27 +2,60 @@ import diskUtils
 import hashlib
 import socket
 
-class fingerTable():
+class Node():
+    thisNode = ()
+    fingers = []
+
+    def createNode(self):
+        ip = socket.gethostbyname(socket.gethostname())
+        key = hash(ip)
+        self.thisNode = (key, ip)
+        return self
+
+    def addFinger(self, ip):
+        key = hash(ip)
+        
+        
+
+class Table():
     entry = []
 
     def createTable(self):
         ip = socket.gethostbyname(socket.gethostname())
         for a in range(diskUtils.blockNum):
             key = hash(ip + ':' + str(a))
-            self.entry.append((key, ip))
+            node = Node()
+            node.thisNode = (key, ip)
+            self.entry.append(node)
         return self
 
     def addEntry(self, ip, block):
-        key = hash(ip + ':' + str(block))
-        self.entry.append((key, ip))
+        for a in range(block):
+            key = hash(ip + str(a))
+            loc = self.find(key)
+            if loc > -1:
+                entry[loc].succesor = (key, ip)
         return self
+
+    def find(self, key):
+        for a in range(diskUtils.blockNum):
+            try:
+                if self.entry[a].thisNode[0] < key and self.entry[a + 1].thisNode[0] > key:
+                    return a
+            except:
+                return -1
+
+    
 
 
 def hash(data):
-    return hashlib.sha1(data)
+    return int(hashlib.sha1(data).digest()[:10], 16)
 
 if __name__ == "__main__":
-    f = fingerTable()
-    f.createTable()
-    for a in f.entry:
-        print(str(a))
+    t = Table()
+    t.createTable()
+    t.addEntry('192.168.7.1', 10)
+
+    for a in t.entry:
+        print(a.thisNode)
+        print(a.succesor)
