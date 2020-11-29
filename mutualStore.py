7831176bd33db5dd4b11fe8807c9c5c2cdef69ab
jmlemaster86 <jmlemaster86@gmail.com>
2020-11-29 21:55:10 +0000

    Basic debug and testing

--- mutualStore.py
+++ mutualStore.py
@@ -3,6 +3,7 @@ import diskUtils as DISK
 import disHash as CHORD
 import sys
 import os
+import time
 
 def storeFile(data):
     block = bytearray(DISK.blockSize)
@@ -11,6 +12,8 @@ def storeFile(data):
         for i in range(DISK.blockSize):
             block[i] = data[a]
             a += 1
+            if a >= len(data):
+                break
         key = CHORD.hash(block)
         loc = CON.chord.inRange(key)
         if(loc > -1):
@@ -24,11 +27,19 @@ def retrieveFile(name):
     return None
 
 if __name__ == "__main__":
+    if(not os.path.exists("disk.bin")):
+        DISK.fdisk()
     pid = os.fork()
     if(pid == 0):
+        time.sleep(2)
         if(len(sys.argv) > 1):
             if(sys.argv[1] == 'store'):
                 with open(sys.argv[2], 'rb') as file:
-                    storeFile(file)
+                    data = bytearray()
+                    byte = file.read(1)
+                    while byte:
+                        data.append(byte[0])
+                        byte = file.read(1)
+                    storeFile(data)
     else:
         CON.initializeServerConnection()
